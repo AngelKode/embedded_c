35b7a228822017f2918ce62a34af27eb356622da
AngelKode <200angel@live.com.mx>
2023-04-20 16:50:20 -0600

    Malloc use examples

--- relearning/relearning.c
+++ relearning/relearning.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main() {
 	
-	
-	struct Plano {
-		float x;
-		float y;
-	};
+	//Assign dynamically memory
+	//We cast malloc generic void response to int*, static size of 1
+	int* dato1 = (int*) malloc(sizeof(int));
 
-	struct Plano var, *ptr;
+	//We cast malloc generic void response to int*, dynamic size depending user entry
+	int integers_count = 0, *integers_list = NULL;
 
-	var.x = 10.2;
-	var.y = 20.1;
-	ptr = &var;
+	printf_s("Enter size of the list: ");
+	scanf_s("%d", &integers_count);
 
-	ptr->x = 5.2;
-	ptr->y = 10.4;
+	integers_list = (int*) malloc(sizeof(int) * integers_count);
 
-	printf_s("%.2f %.2f", var.x, var.y);
+	//Print dynamic memory size reserved
+	printf_s("Array size: %d\n",(int) sizeof(integers_list) * integers_count);
+	for (int listIndex = 0; listIndex < integers_count; listIndex++) {
+		printf_s("Enter integer value: ");
+		scanf_s("%d", &integers_list[listIndex]);
+	}
+
+	//Print all enter values
+	for (int listIndex = 0; listIndex < integers_count; listIndex++) {
+		printf_s("Integer value: %d\n", integers_list[listIndex]);
+	}
+
+	//Free reserved space in memory
+	free(integers_list);
+	integers_list = NULL;
 
 	return 0;
 }
