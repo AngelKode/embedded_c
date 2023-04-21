66fcd759d1d159f088bbe90c8d1846689ef1f2d8
AngelKode <200angel@live.com.mx>
2023-04-20 23:41:42 -0600

    Rock, paper, scisors start. Main game not implemented yet

--- relearning/relearning.c
+++ relearning/relearning.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+struct Player {
+	char* name;
+	int victoryCount;
+	int loseCount;
+};
 
 int main() {
 	
 	//Assign dynamically memory
 	//We cast malloc generic void response to int*, static size of 1
+	/*
 	int* dato1 = (int*) malloc(sizeof(int));
 
 	//We cast malloc generic void response to int*, dynamic size depending user entry
@@ -12,7 +21,7 @@ int main() {
 
 	printf_s("Enter size of the list: ");
 	scanf_s("%d", &integers_count);
-
+	
 	integers_list = (int*) malloc(sizeof(int) * integers_count);
 
 	//Print dynamic memory size reserved
@@ -21,7 +30,7 @@ int main() {
 		printf_s("Enter integer value: ");
 		scanf_s("%d", &integers_list[listIndex]);
 	}
-
+	
 	//Print all enter values
 	for (int listIndex = 0; listIndex < integers_count; listIndex++) {
 		printf_s("Integer value: %d\n", integers_list[listIndex]);
@@ -30,6 +39,66 @@ int main() {
 	//Free reserved space in memory
 	free(integers_list);
 	integers_list = NULL;
+	*/
+
+	//Rock, paper, scisor game
+	srand(time(NULL));
+	bool isGameActive = true;
+	int userSelection = 0;
+	struct Player* user = (struct Player *) malloc(sizeof(struct Player));
+	struct Player* computer = (struct Player*) malloc(sizeof(struct Player));
+
+	if (user != NULL) {
+		user->name = "Angel";
+		user->victoryCount = 0;
+		user->loseCount = 0;
+	}
+	
+	if (computer != NULL) {
+		computer->name = "Computadora";
+		computer->victoryCount = 0;
+		computer->loseCount = 0;
+	}
+
+	do {
+		system("cls");
+		printf_s("-----ROCK, PAPER, SCISORS------\n");
+		printf_s("OPTIONS:\n");
+		printf_s("1. Start/Keep playing\n");
+		printf_s("2. Show partial score\n");
+		printf_s("3. Exit\n");
+		printf_s("OPCION ELEGIDA: ");
+		scanf_s("%d", &userSelection);
+		_flushall();//Clearing buffer
+
+		switch (userSelection){
+			case 1: {
+
+				break;
+			};
+			case 2: {
+				system("cls");
+				printf_s("----SCORE----\n");
+				printf_s("USER %s -> Victory Count=%d - Lose Count=%d\n", user->name, user->victoryCount, user->loseCount);
+				printf_s("USER %s -> Victory Count=%d - Lose Count=%d\n", computer->name, computer->victoryCount, computer->loseCount);
+				printf_s("1. GO TO PRINCIPAL MENU\n");
+				printf_s("2. EXIT\n");
+				printf_s("OPCION ELEGIDA: ");
+				scanf_s("%d", &userSelection);
+
+				if (userSelection == 2) {
+					exit(EXIT_SUCCESS);
+				}
+				break;
+			};
+			default: {
+				isGameActive = false;
+				free(user); user = NULL;
+				free(computer); computer = NULL;
+				break;
+			}
+		}
+	} while (isGameActive);
 
 	return 0;
 }
